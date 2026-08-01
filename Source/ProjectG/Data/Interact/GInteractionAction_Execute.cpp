c3cda4ae55f80f03a9751517eefb76bd36936d99
songsuyoung <tndud3999@naver.com>
2026-08-01 14:02:41 +0900

    GInteractionAction_Execute 구현 추가 - InternalInteract 호출

---
+++ Source/ProjectG/Data/Interact/GInteractionAction_Execute.cpp
@@ -0,0 +1,16 @@
+// Source/ProjectG/Data/Interact/GInteractionAction_Execute.cpp
+#include "Data/Interact/GInteractionAction_Execute.h"
+#include "Gimmick/GInteractableActor.h"
+
+void UGInteractionAction_Execute::Execute(AActor* OwnerActor, AActor* TargetActor)
+{
+	AGInteractableActor* Interactable = Cast<AGInteractableActor>(OwnerActor);
+	if (false == IsValid(Interactable))
+	{
+		Finish();
+		return;
+	}
+
+	Interactable->InternalInteract(TargetActor);
+	Finish();
+}
